dff700e809d696fa4a19a8ea9faa4d300b9a7538
Paulo <phm.paulomorais@gmail.com>
2019-10-07 23:50:55 -0300

    Projeto air-cnc-backend

---
+++ src/controllers/ApprovalController.js
@@ -0,0 +1,21 @@
+const Booking = require('../models/Booking');
+
+module.exports = {
+    async store(req, res) {
+        const { booking_id } = req.params;
+
+        const booking = await Booking.findById(booking_id).populate('spot');
+
+        booking.approved = true;
+
+        await booking.save();
+
+        const bookinkUserSocket = req.connectedUsers[booking.user];
+
+        if(bookinkUserSocket) {
+            req.io.to(bookinkUserSocket).emit('booking_response', booking);
+        }
+
+        return res.json(booking);
+    }
+};
